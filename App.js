e9963564049df618aec5afbbeee2d8be9e3a368b
Angel Landkoer <angel.landkoer09@gmail.com>
2023-02-12 17:31:27 -0500

    Cambiendo el texto original, agregando un texto de bienvenida y agregando script necesario

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola, Coder!</Text>
+      <Text>Welcome to The-burger-farm</Text>
       <StatusBar style="auto" />
     </View>
   );
