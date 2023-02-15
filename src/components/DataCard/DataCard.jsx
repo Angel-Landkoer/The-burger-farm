318a4c649f1b47d2513040ec49a2537ec854efc5
Angel Landkoer <angel.landkoer09@gmail.com>
2023-02-15 17:12:21 -0500

    creando un componente

---
+++ src/components/DataCard/DataCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+export function DataCard({ data }) {
+  return (
+    <View>
+      <Text>{``}</Text>
+      <Image source={{ uri: "" }} />
+      <Text>{``}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+const {} = styles;
