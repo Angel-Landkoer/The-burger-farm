5741144e8cf57ed7ade2eba8cc76d4b0910d83c1
Angel Landkoer <angel.landkoer09@gmail.com>
2023-02-16 10:23:23 -0500

    creando estructura inicial del componente

--- src/components/CartProduct/CartProduct.jsx
+++ src/components/CartProduct/CartProduct.jsx
@@ -1,10 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 export function CartProduct() {
   return (
     <View>
-      <Text>CartProduct</Text>
+      <View>
+        <Image
+          source={{
+            uri: "",
+          }}
+        />
+      </View>
+      <View>
+        <Button color={`red`} onPress={() => null} title="-" />
+        <Text>{}</Text>
+        <Button color={`green`} onPress={() => null} title="+" />
+      </View>
+
+      <View>
+        <Text>Product</Text>
+        <Text>Price</Text>
+      </View>
     </View>
   );
 }
