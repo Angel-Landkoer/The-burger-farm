cb25e856cad4d934ac9b8eedd2e234011592d44f
Angel Landkoer <angel.landkoer09@gmail.com>
2023-03-01 09:49:05 -0500

    Creando componente de notas sus estilos

---
+++ src/components/Notes/Notes.jsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+import { themes } from "../../styles/themes";
+
+export function Notes({ data, onPress }) {
+  return (
+    <View style={[container, tertiaryBorderColor]}>
+      <Text style={[styleText, textXl, textCenter, fontBold, senaryColor]}>
+        {data.data}
+      </Text>
+      <Button title="deleted Info" onPress={onPress} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 220,
+    borderWidth: 2,
+    padding: 10,
+    marginVertical: 50,
+
+    borderRadius: 10,
+  },
+  styleText: {
+    margin: 10,
+    padding: 10,
+  },
+});
+
+const { container, styleText } = styles;
+const { tertiaryBorderColor, senaryColor, textCenter, fontBold, textXl } =
+  themes;
