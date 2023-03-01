ebc910514e8e7a0fb88702478f66cb1e8f1e9d42
Angel Landkoer <angel.landkoer09@gmail.com>
2023-03-01 09:50:33 -0500

    conectando componente, eventos, hook, estilos, para dar funcionalidad crear notas

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
+import { useState } from 'react';
+import { Button, FlatList, TextInput, SafeAreaView, StyleSheet, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { themes } from './src/styles/themes';
+import { Notes } from './src/components/Notes/Notes';
 
 export default function App() {
+  const [formData, setFormData] = useState('')
+  const [card, setCard] = useState([])
+  const [remove, setRemove] = useState([])
+
+  const handleChangeValue = (e) => setFormData(e)
+
+  const resetInfo = () => setFormData('')
+
+  const list = () => {
+    setCard([...card, { date: `${Date.now()}`, data: formData }])
+    resetInfo()
+  }
+
+  const deleted = (id) => {
+    const index = card.findIndex(item => item.date === id)
+
+    const removeIndex = card.splice(index, 1)
+    setRemove(removeIndex)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hola, Coder!</Text>
-      <Text>Welcome to The-burger-farm</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[primaryBackground, container]} >
+      <StatusBar style='light' backgroundColor='black' />
+      <Text style={[title]} >{`NOTES: ${card.length}`}</Text>
+      <TextInput
+        style={stylesInput}
+        inputMode="decimal"
+        placeholder='Write text'
+        placeholderTextColor={"#BDBFC3"}
+        onChangeText={handleChangeValue}
+        value={formData}
+      />
+      <Button title='Send Info' onPress={list} />
+      <FlatList
+        data={card}
+        renderItem={items => <Notes data={items.item} onPress={() => deleted(items.item.date)} />}
+        keyExtractor={item => item.date}
+      />
+    </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: 20,
+    paddingVertical: 50,
+    marginVertical: 20,
+    backgroundColor: "#161E20"
   },
-});
+  stylesInput: {
+    width: 250,
+    height: 40,
+    borderWidth: 2,
+    marginVertical: 20,
+    paddingHorizontal: 5,
+
+    textAlign: "center",
+
+    color: "#BDBFC3",
+    borderRadius: 20,
+    borderColor: "#ffff",
+
+  },
+  title: {
+    fontSize: 30,
+    textAlign: "center",
+
+    color: "#BDBFC3",
+  }
+})
+const { container, stylesInput, title } = styles
+const { primaryBackground } = themes
