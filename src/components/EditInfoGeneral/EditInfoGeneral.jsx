96b0f66b9303f9ac77bf88308332fae0533077fd
Angel Landkoer <angel.landkoer09@gmail.com>
2023-03-03 13:35:26 -0500

    creando componente y estilandolo

---
+++ src/components/EditInfoGeneral/EditInfoGeneral.jsx
@@ -0,0 +1,112 @@
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+
+import { ButtonSaveClose } from "../ButtonSaveClose/ButtonSaveClose";
+import { themes } from "../../styles/themes";
+
+export function EditInfoGeneral() {
+  return (
+    <View style={[container]}>
+      <View style={[contentInput, primaryBorderColor]}>
+        <TextInput
+          style={[
+            input,
+            primaryColor,
+            textBase,
+            fontBold,
+            tertiaryBorderColor,
+          ]}
+          placeholder="Name"
+          placeholderTextColor={tertiaryColor.color}
+          inputMode="text"
+          keyboardType="default"
+          onChangeText={null}
+        />
+        <TextInput
+          style={[
+            input,
+            tertiaryColor,
+            textBase,
+            fontBold,
+            tertiaryBorderColor,
+          ]}
+          placeholder="Last Name"
+          placeholderTextColor={tertiaryColor.color}
+          inputMode="text"
+          keyboardType="default"
+          onChangeText={null}
+        />
+        <TextInput
+          style={[
+            input,
+            tertiaryColor,
+            textBase,
+            fontBold,
+            tertiaryBorderColor,
+          ]}
+          placeholder="Phone"
+          placeholderTextColor={tertiaryColor.color}
+          inputMode="numeric"
+          keyboardType="phone-pad"
+          onChangeText={null}
+        />
+        <TextInput
+          style={[
+            input,
+            tertiaryColor,
+            textBase,
+            fontBold,
+            tertiaryBorderColor,
+          ]}
+          placeholder="Email"
+          placeholderTextColor={tertiaryColor.color}
+          inputMode="email"
+          keyboardType="email-address"
+          onChangeText={null}
+        />
+      </View>
+      <View style={[contentBtns]}>
+        <ButtonSaveClose />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-around",
+    alignItems: "center",
+
+    width: "100%",
+    height: "100%",
+  },
+  contentInput: {
+    width: "100%",
+    padding: 12,
+    borderWidth: 1,
+
+    borderRadius: 15,
+  },
+  input: {
+    width: "100%",
+    padding: 5,
+    marginVertical: 10,
+
+    borderBottomWidth: 1,
+  },
+  contentBtns: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+const { input, container, contentBtns, contentInput } = styles;
+
+const {
+  primaryColor,
+  primaryBorderColor,
+  textBase,
+  fontBold,
+  tertiaryColor,
+  tertiaryBorderColor,
+} = themes;
