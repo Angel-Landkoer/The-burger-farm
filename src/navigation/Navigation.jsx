91a1ce51a3db89c5acafa5e4e0dfffd34bdf286d
Angel Landkoer <angel.landkoer09@gmail.com>
2023-03-13 09:12:58 -0500

    Creando archivo de navegacion y exportandolo

---
+++ src/navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Products } from "../screens/Products/Products";
+import { MyCart } from "../screens/MyCart/MyCart";
+import { Login } from "../screens/Login/Login";
+import { MyOrders } from "../screens/MyOrders/MyOrders";
+import { FindUs } from "../screens/FindUs/FindUs";
+
+export function Navigation() {
+  const { Navigator, Screen } = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Navigator>
+        <Screen name="Products" component={Products} />
+        <Screen name="MyCard" component={MyCart} />
+        <Screen name="MyData" component={Login} />
+        <Screen name="MyOrders" component={MyOrders} />
+        <Screen name="FindUs" component={FindUs} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
