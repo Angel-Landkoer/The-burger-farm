c76f7bbaf4698a9684ca103631a781144d0b97c3
Angel Landkoer <angel.landkoer09@gmail.com>
2023-03-13 09:14:28 -0500

    Importando archivo de navegacion y eliminando importaciones no usadas

--- App.js
+++ App.js
@@ -1,42 +1,13 @@
 import { StyleSheet, View } from 'react-native';
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Products } from './src/screens/Products/Products'
-import { MyCart } from './src/screens/MyCart/MyCart'
-import { Login } from './src/screens/Login/Login'
-import { MyOrders } from './src/screens/MyOrders/MyOrders'
-import { FindUs } from './src/screens/FindUs/FindUs'
+import React from 'react';
+import { Navigation } from './src/navigation/Navigation'
 // import { CustomText } from './src/components/CustomText/CustomText';
 
 export default function App() {
 
-  const [screens, setScreens] = useState({
-    home: true,
-    cart: false,
-    data: false,
-    orders: false,
-    findUs: false
-  })
-
-  const { home, cart, data, orders, findUs } = screens
-
   return (
-    <View style={styles.container}>
-      {home && <Products screens={screens} setScreens={setScreens} />}
-      {cart && <MyCart screens={screens} setScreens={setScreens} />}
-      {data && <Login screens={screens} setScreens={setScreens} />}
-      {orders && <MyOrders screens={screens} setScreens={setScreens} />}
-      {findUs && <FindUs screens={screens} setScreens={setScreens} />}
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Navigation />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
