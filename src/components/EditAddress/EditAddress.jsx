729c060f4ed61ecb817381dc673a30be83e18eec
Angel Landkoer <angel.landkoer09@gmail.com>
2023-03-20 16:45:24 -0500

    Implementando useRedurcer para guardar las respuestas en los formularios, integrado el modal y creando y enviando función a un componente Btn

--- src/components/EditAddress/EditAddress.jsx
+++ src/components/EditAddress/EditAddress.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import React, { useReducer } from "react";
+import { Pressable, StyleSheet, TextInput, View } from "react-native";
 import SelectDropdown from "react-native-select-dropdown";
 import { themes } from "../../styles/themes";
 import { ButtonSaveClose } from "../ButtonSaveClose/ButtonSaveClose";
 import { CustomText } from "../CustomText/CustomText";
+import { Modall } from "../Modal/Modall";
 
 // dato de los ciudades and departamentos
 
@@ -20,9 +21,27 @@ const countries = [
 ];
 
 const seletions = ["select", "Street", "Race"];
-//
 
 export function EditAddress() {
+  const [state, dispatch] = useReducer(reducer, inicialState);
+
+  const onSaveData = () => {
+    const formulariesLength = [
+      state.inputCity,
+      state.inputRace,
+      state.inputDirectionFirst,
+      state.inputDirectionSecond,
+      state.inputDirectionThird,
+      state.inputDistrict,
+      state.inputAdditional,
+    ];
+
+    const maxLength = formulariesLength.every((item) => item.length > 0);
+
+    !maxLength && dispatch({ type: "CHANGETOGGLE" });
+    maxLength && (dispatch({ type: "RESET" }), console.warn("Confirm Data"));
+  };
+
   return (
     <View style={[container]}>
       <View style={[allForms, primaryBorderColor]}>
@@ -31,7 +50,9 @@ export function EditAddress() {
             buttonStyle={selectDropdowns}
             buttonTextStyle={selectDropdownText}
             data={countries}
-            onSelect={(selectedItem, index) => console.log(selectedItem, index)}
+            onSelect={(selectedItem, index) =>
+              dispatch({ type: "INPUTCITY", payload: selectedItem })
+            }
             buttonTextAfterSelection={(selectedItem, index) => selectedItem}
             rowTextForSelection={(item, index) => item}
           />
@@ -39,12 +60,13 @@ export function EditAddress() {
             buttonStyle={selectDropdowns}
             buttonTextStyle={selectDropdownText}
             data={seletions}
-            onSelect={(selectedItem, i) => console.log(selectedItem, i)}
+            onSelect={(selectedItem, i) =>
+              dispatch({ type: "INPUTRACE", payload: selectedItem })
+            }
             buttonTextAfterSelection={(selectedItem, i) => selectedItem}
             rowTextForSelection={(item, i) => item}
           />
         </View>
-
         <View
           style={{
             alignItems: "center",
@@ -62,6 +84,10 @@ export function EditAddress() {
               placeholder="00"
               placeholderTextColor={secondaryColor.color}
               inputMode="text"
+              value={state.inputDirectionFirst}
+              onChangeText={(e) =>
+                dispatch({ type: "DIRECTION-1", payload: e })
+              }
             />
           </View>
           <View style={[contentInput, primaryBorderColor]}>
@@ -74,6 +100,10 @@ export function EditAddress() {
               placeholder="00"
               placeholderTextColor={secondaryColor.color}
               inputMode="numeric"
+              value={state.inputDirectionSecond}
+              onChangeText={(e) =>
+                dispatch({ type: "DIRECTION-2", payload: e })
+              }
             />
           </View>
           <View style={[contentInput, primaryBorderColor]}>
@@ -86,10 +116,13 @@ export function EditAddress() {
               placeholder="00"
               placeholderTextColor={secondaryColor.color}
               inputMode="text"
+              value={state.inputDirectionThird}
+              onChangeText={(e) =>
+                dispatch({ type: "DIRECTION-3", payload: e })
+              }
             />
           </View>
         </View>
-
         <View style={[contentDataForm]}>
           <TextInput
             style={[
@@ -101,6 +134,10 @@ export function EditAddress() {
             ]}
             placeholder="District"
             placeholderTextColor={secondaryColor.color}
+            value={state.inputDistrict}
+            onChangeText={(e) =>
+              dispatch({ type: "INPUTDISTRICT", payload: e })
+            }
           />
           <TextInput
             style={[
@@ -112,15 +149,77 @@ export function EditAddress() {
             ]}
             placeholder="Additional Infotmation"
             placeholderTextColor={secondaryColor.color}
+            value={state.inputAdditional}
+            onChangeText={(e) =>
+              dispatch({ type: "INPUTADDITIONAL", payload: e })
+            }
           />
         </View>
       </View>
+      <Modall text={"Please fill in all fields"} state={state.toggleModal}>
+        <Pressable
+          style={[modalBtn, septenaryBackground]}
+          onPress={() => dispatch({ type: "CHANGETOGGLE" })}
+        >
+          <CustomText
+            fontF={"semiBold"}
+            style={[textModalBtn, senaryColor, textLg]}
+          >
+            Accept
+          </CustomText>
+        </Pressable>
+      </Modall>
       <View style={[contentBtns]}>
-        <ButtonSaveClose />
+        <ButtonSaveClose onSaveData={onSaveData} />
       </View>
     </View>
   );
 }
+
+const inicialState = {
+  inputCity: "",
+  inputRace: "",
+  inputDirectionFirst: "",
+  inputDirectionSecond: "",
+  inputDirectionThird: "",
+  inputDistrict: "",
+  inputAdditional: "",
+  toggleModal: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INPUTCITY":
+      return { ...state, inputCity: action.payload };
+    case "INPUTRACE":
+      return { ...state, inputRace: action.payload };
+    case "DIRECTION-1":
+      return { ...state, inputDirectionFirst: action.payload };
+    case "DIRECTION-2":
+      return { ...state, inputDirectionSecond: action.payload };
+    case "DIRECTION-3":
+      return { ...state, inputDirectionThird: action.payload };
+    case "INPUTDISTRICT":
+      return { ...state, inputDistrict: action.payload };
+    case "INPUTADDITIONAL":
+      return { ...state, inputAdditional: action.payload };
+    case "RESET":
+      return {
+        ...state,
+        inputCity: "",
+        inputRace: "",
+        inputDirectionFirst: "",
+        inputDirectionSecond: "",
+        inputDirectionThird: "",
+        inputDistrict: "",
+        inputAdditional: "",
+      };
+    case "CHANGETOGGLE":
+      return { ...state, toggleModal: !state.toggleModal };
+    default:
+      return state;
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -179,6 +278,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   adicionalInpunt: { borderBottomWidth: 1, padding: 5, marginVertical: 10 },
+  modalBtn: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 100,
+    height: 40,
+
+    borderRadius: 20,
+  },
+  textModalBtn: {},
 });
 
 const {
@@ -193,6 +301,15 @@ const {
   contentFormDropdown,
   selectDropdowns,
   selectDropdownText,
+  modalBtn,
+  textModalBtn,
 } = styles;
 
-const { textLg, secondaryColor, fontBold, primaryBorderColor } = themes;
+const {
+  textLg,
+  secondaryColor,
+  fontBold,
+  primaryBorderColor,
+  senaryColor,
+  septenaryBackground,
+} = themes;
