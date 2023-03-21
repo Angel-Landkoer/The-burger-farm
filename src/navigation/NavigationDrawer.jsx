46c6d5999bc602c73b8c76d332e628e76afd2109
Angel Landkoer <angel.landkoer09@gmail.com>
2023-03-21 11:00:59 -0500

    Exportando las navegaciones Stack en la navegacion de Drawer

--- src/navigation/NavigationDrawer.jsx
+++ src/navigation/NavigationDrawer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from '@react-navigation/drawer'
-import { FindUs } from '../screens/FindUs/FindUs'
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { ProductsStackNavigation } from "./ProductsStackNavigation";
+import { CartStackNavigation } from "./CartStackNavigation";
+import { DataStackNavigation } from "./DataStackNavigation";
+import { OrderStackNavigation } from "./OrderStackNavigation";
+import { FindUs } from "../screens/FindUs/FindUs";
 
 export function NavigationDrawer() {
   const { Navigator, Screen } = createDrawerNavigator();
@@ -9,6 +13,26 @@ export function NavigationDrawer() {
   return (
     <NavigationContainer>
       <Navigator>
+        <Screen
+          name="ProductDrawer"
+          component={ProductsStackNavigation}
+          options={{ title: "Products" }}
+        />
+        <Screen
+          name="CartDrawer"
+          component={CartStackNavigation}
+          options={{ title: "My Cart" }}
+        />
+        <Screen
+          name="DataDrawer"
+          component={DataStackNavigation}
+          options={{ title: "My Data" }}
+        />
+        <Screen
+          name="OrderDrawer"
+          component={OrderStackNavigation}
+          options={{ title: "My Order" }}
+        />
         <Screen
           name="FindUs"
           component={FindUs}
