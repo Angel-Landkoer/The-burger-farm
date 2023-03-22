c58697f5d90720b21e854335ad5f7c8854ffd8be
Angel Landkoer <angel.landkoer09@gmail.com>
2023-03-22 11:40:41 -0500

    Estilando navegacion principal y agregandole iconos

--- src/navigation/NavigationDrawer.jsx
+++ src/navigation/NavigationDrawer.jsx
@@ -1,44 +1,103 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { FontAwesome5, Ionicons } from "@expo/vector-icons";
 import { ProductsStackNavigation } from "./ProductsStackNavigation";
 import { CartStackNavigation } from "./CartStackNavigation";
 import { DataStackNavigation } from "./DataStackNavigation";
 import { OrderStackNavigation } from "./OrderStackNavigation";
 import { FindUs } from "../screens/FindUs/FindUs";
+import { themes } from "../styles/themes";
+import { StyleSheet } from "react-native";
 
 export function NavigationDrawer() {
   const { Navigator, Screen } = createDrawerNavigator();
 
   return (
     <NavigationContainer>
-      <Navigator>
+      <Navigator
+        screenOptions={{
+          drawerLabelStyle: [primaryColor, textXl, fontBold],
+          drawerStyle: [primaryBackground],
+          headerStyle: [primaryBackground, headerContain],
+          headerTitleStyle: [text2Xl, primaryColor, fontBold],
+          headerTitleAlign: "center",
+        }}
+      >
         <Screen
           name="ProductDrawer"
           component={ProductsStackNavigation}
-          options={{ title: "Products" }}
+          options={{
+            title: "Products",
+            drawerIcon: ({ color, size }) => (
+              <FontAwesome5 name="hamburger" size={size} color={color} />
+            ),
+          }}
         />
         <Screen
           name="CartDrawer"
           component={CartStackNavigation}
-          options={{ title: "My Cart" }}
+          options={{
+            title: "My Cart",
+            drawerIcon: ({ color, size }) => (
+              <FontAwesome5 name="shopping-cart" size={size} color={color} />
+            ),
+          }}
         />
         <Screen
           name="DataDrawer"
           component={DataStackNavigation}
-          options={{ title: "My Data" }}
+          options={{
+            title: "My Data",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person-sharp" size={size} color={color} />
+            ),
+          }}
         />
         <Screen
           name="OrderDrawer"
           component={OrderStackNavigation}
-          options={{ title: "My Order" }}
+          options={{
+            title: "My Order",
+            headerShown: false,
+            drawerIcon: ({ size, color }) => (
+              <FontAwesome5
+                name="file-invoice-dollar"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
         />
         <Screen
           name="FindUs"
           component={FindUs}
-          options={{ title: " Find Us" }}
+          options={{
+            title: " Find Us",
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="map" size={size} color={color} />
+            ),
+          }}
         />
       </Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  headerContain: {
+    height: 100,
+    borderBottomWidth: 3,
+  },
+});
+
+const { headerContain } = styles;
+
+const {
+  primaryColor,
+  textXl,
+  text2Xl,
+  fontBold,
+  primaryBackground,
+  textCenter,
+} = themes;
