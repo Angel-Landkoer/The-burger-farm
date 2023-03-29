1cef514537eb03adfa9df5aa3bad2da3e75ba28e
Angel Landkoer <angel.landkoer09@gmail.com>
2023-03-29 16:33:17 -0500

    Agregando acciones que cambien el estado

--- src/store/cartSistem/reducers/cartSistem.reducer.js
+++ src/store/cartSistem/reducers/cartSistem.reducer.js
@@ -1,20 +1,27 @@
-import { ADD_CART, DELETED_ALL, DELETED_ITEM, TOTAL_AMOUNT } from '../actions/cartSistem.action'
+import { ADD_CART, DELETED_ALL, DELETED_ITEM, CART_LENGTH } from '../actions/cartSistem.action'
 
 const initionalState = {
-  cart: [],
+  cartItems: [],
   sizeCart: 0,
   total: 0
 }
 
 export const cartReducer = (state = initionalState, action) => {
   if (action.type == ADD_CART) {
-    const newCart = [{a: "queso", b: "soso"}]
-    return { ...state, cart: newCart }
+    const newCart = [...state.cartItems, action.item]
+    return { ...state, cartItems: newCart }
   }
 
-  if (action.type == DELETED_ITEM) return
-  if (action.type == DELETED_ALL) return
-  if (action.type == TOTAL_AMOUNT) return
+  if (action.type == DELETED_ITEM) {
+    const existing = state.cartItems.some(item => item.name == action.deletedItem.name)
+    if (!existing) return { ...state, cartItems: state.cartItems }
+    const findIndex = state.cartItems.findIndex(item => item.name == action.deletedItem.name)
+    const removeItem = state.cartItems.splice(findIndex, 0)
+    return { ...state, cartItems: removeItem }
+  }
+  if (action.type == DELETED_ALL) return { ...state, cartItems: [] }
+  if (action.type == CART_LENGTH) return { ...state, sizeCart: state.cartItems.length }
+  // if (action.type == TOTAL_AMOUNT) return
   // if(action.type == "") return
   return state
 }
