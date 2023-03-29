818fb576f052f3e548d9bc0b925bbcff433a00b3
Angel Landkoer <angel.landkoer09@gmail.com>
2023-03-29 18:16:20 -0500

    Retornando el objeto que no se retornaba

--- src/store/cartSistem/actions/cartSistem.action.js
+++ src/store/cartSistem/actions/cartSistem.action.js
@@ -4,9 +4,9 @@ export const DELETED_ALL = "@DELETED_ALL"
 export const CART_LENGTH = "@CART_LENGTH"
 // export const TOTAL_AMOUNT = "@TOTAL_AMOUNT"
 
-export const addItemCart = (item) => { type: ADD_CART, item }
+export const addItemCart = (item) => ({ type: ADD_CART, item })
 
-export const deletedItemCart = (deletedItem) => { type: DELETED_ITEM, deletedItem }
+export const deletedItemCart = (deletedItem) => ({ type: DELETED_ITEM, deletedItem })
 
 export const deletedAll = () => { type: DELETED_ALL }
 
--- src/store/globalData/actions/globalData.action.js
+++ src/store/globalData/actions/globalData.action.js
@@ -1,6 +1,6 @@
 export const PRODUCT_CATEGORY = "@PRODUCT_CATEGORY"
 
-export const filterCategory = (numCategory) => { type: PRODUCT_CATEGORY, numCategory }
+export const filterCategory = (numCategory) => ({ type: PRODUCT_CATEGORY, numCategory })
 
 // data products - desde Firebase
 // data de ordenes - desde Firebase
