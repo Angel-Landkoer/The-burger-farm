0b5140c33863eaad794196761ecc7fc410039d64
Angel Landkoer <angel.landkoer09@gmail.com>
2023-03-30 11:55:06 -0500

    Eliminando funciones innecesarias y sus constantes

--- src/store/cartSistem/actions/cartSistem.action.js
+++ src/store/cartSistem/actions/cartSistem.action.js
@@ -1,16 +1,10 @@
 export const ADD_CART = "@ADD_CART"
 export const DELETED_ITEM = "@DELETED_ITEM"
-export const DELETED_ALL = "@DELETED_ALL"
-export const CART_LENGTH = "@CART_LENGTH"
 // export const TOTAL_AMOUNT = "@TOTAL_AMOUNT"
 
 export const addItemCart = (item) => ({ type: ADD_CART, item })
 
 export const deletedItemCart = (deletedItem) => ({ type: DELETED_ITEM, deletedItem })
-
-export const deletedAll = () => { type: DELETED_ALL }
-
-export const cartLength = () => { type: CART_LENGTH }
 
 // export const totalAmount = () => { type: TOTAL_AMOUNT }
 
