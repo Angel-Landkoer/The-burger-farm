f2d5dc5a5cdd95238973a07d5671bf0662f473ba
Angel Landkoer <angel.landkoer09@gmail.com>
2023-04-03 16:37:12 -0500

    Eliminado contenedor global de la navegación e importando en la vista global de las navegaciones y creando icono del carro de compras con sus estilos

--- src/navigation/NavigationDrawer.jsx
+++ src/navigation/NavigationDrawer.jsx
@@ -1,7 +1,8 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { useSelector } from "react-redux";
 import { FontAwesome5, Ionicons } from "@expo/vector-icons";
 import { ProductsStackNavigation } from "./ProductsStackNavigation";
 import { CartStackNavigation } from "./CartStackNavigation";
@@ -9,82 +10,105 @@ import { DataStackNavigation } from "./DataStackNavigation";
 import { OrderStackNavigation } from "./OrderStackNavigation";
 import { FindUsStackNavigation } from "./FindUsStackNavigation";
 import { themes } from "../styles/themes";
+import { CustomText } from "../components/CustomText/CustomText";
 
 export function NavigationDrawer() {
   const { Navigator, Screen } = createDrawerNavigator();
 
   return (
-    <NavigationContainer>
-      <Navigator
-        screenOptions={{
-          drawerLabelStyle: [primaryColor, textXl, fontBold],
-          drawerStyle: [primaryBackground],
-          headerStyle: [primaryBackground, headerContain],
-          headerTitleStyle: [text3Xl, primaryColor, fontBold],
-          headerTitleAlign: "center",
+    <Navigator
+      screenOptions={{
+        drawerLabelStyle: [primaryColor, textXl, fontBold],
+        drawerStyle: [primaryBackground],
+        headerStyle: [primaryBackground, headerContain],
+        headerTitleStyle: [text3Xl, primaryColor, fontBold],
+        headerTitleAlign: "center",
+      }}
+    >
+      <Screen
+        name="ProductDrawer"
+        component={ProductsStackNavigation}
+        options={{
+          title: "Products",
+          headerShown: true,
+          headerRight: () => <IconCartLength />,
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome5 name="hamburger" size={size} color={color} />
+          ),
         }}
-      >
-        <Screen
-          name="ProductDrawer"
-          component={ProductsStackNavigation}
-          options={{
-            title: "Products",
-            headerShown: true,
-            drawerIcon: ({ color, size }) => (
-              <FontAwesome5 name="hamburger" size={size} color={color} />
-            ),
-          }}
+      />
+      <Screen
+        name="CartDrawer"
+        component={CartStackNavigation}
+        options={{
+          title: "My Cart",
+          headerShown: false,
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome5 name="shopping-cart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="DataDrawer"
+        component={DataStackNavigation}
+        options={{
+          title: "My Data",
+          headerShown: false,
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="person-sharp" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="OrderDrawer"
+        component={OrderStackNavigation}
+        options={{
+          title: "My Order",
+          headerShown: false,
+          drawerIcon: ({ size, color }) => (
+            <FontAwesome5
+              name="file-invoice-dollar"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="FindUpDrawer"
+        component={FindUsStackNavigation}
+        options={{
+          title: "Find Us",
+          headerShown: false,
+          drawerIcon: ({ size, color }) => (
+            <Ionicons name="map" size={size} color={color} />
+          ),
+        }}
+      />
+    </Navigator>
+  );
+}
+
+function IconCartLength() {
+  const lengthCart = useSelector((state) => state.cart.sizeCart);
+  const { navigate } = useNavigation();
+  const route = () => navigate("CartDrawer");
+
+  return (
+    <TouchableWithoutFeedback onPress={route}>
+      <View style={contentLengthCart}>
+        <View style={[contentTextLengthCart]}>
+          <CustomText style={[senaryColor]} fontF={"semiBold"}>
+            {lengthCart}
+          </CustomText>
+        </View>
+        <FontAwesome5
+          name="shopping-cart"
+          size={28}
+          color={primaryColor.color}
         />
-        <Screen
-          name="CartDrawer"
-          component={CartStackNavigation}
-          options={{
-            title: "My Cart",
-            headerShown: false,
-            drawerIcon: ({ color, size }) => (
-              <FontAwesome5 name="shopping-cart" size={size} color={color} />
-            ),
-          }}
-        />
-        <Screen
-          name="DataDrawer"
-          component={DataStackNavigation}
-          options={{
-            title: "My Data",
-            headerShown: false,
-            drawerIcon: ({ color, size }) => (
-              <Ionicons name="person-sharp" size={size} color={color} />
-            ),
-          }}
-        />
-        <Screen
-          name="OrderDrawer"
-          component={OrderStackNavigation}
-          options={{
-            title: "My Order",
-            headerShown: false,
-            drawerIcon: ({ size, color }) => (
-              <FontAwesome5
-                name="file-invoice-dollar"
-                size={size}
-                color={color}
-              />
-            ),
-          }}
-        />
-        <Screen
-          name="FindUpDrawer"
-          component={FindUsStackNavigation}
-          options={{
-            title: "Find Us",
-            headerShown: false,
-            drawerIcon: ({ size, color }) => (
-              <Ionicons name="map" size={size} color={color} />
-            ),
-          }}
-        />
-      </Navigator>
-    </NavigationContainer>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -93,9 +117,27 @@ const styles = StyleSheet.create({
     height: 100,
     borderBottomWidth: 3,
   },
+  contentTextLengthCart: {
+    position: "absolute",
+    top: 1,
+    right: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    width: 18,
+    height: 18,
+    backgroundColor: "red",
+    borderRadius: 20,
+    zIndex: 1,
+  },
+  contentLengthCart: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 84,
+    height: 40,
+  },
 });
 
-const { headerContain } = styles;
+const { headerContain, contentTextLengthCart, contentLengthCart } = styles;
 
 const {
   primaryColor,
@@ -104,4 +146,5 @@ const {
   fontBold,
   primaryBackground,
   textCenter,
+  senaryColor,
 } = themes;
