5e380df1ff694fc7f7a0b730e6d8a2f0ebd0521e
Angel Landkoer <angel.landkoer09@gmail.com>
2023-04-05 11:44:23 -0500

    Agregando llamado a nuevo reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { cartReducer } from './cartSistem/reducers/cartSistem.reducer'
 import { globalDataReducer } from './globalData/reducers/globalData.reducer'
+import { authReducer } from './authUser/reducers/authUser.reducer'
 
 const RootReducers = combineReducers({
   cart: cartReducer,
   data: globalDataReducer,
+  auth: authReducer
 })
 
-export default createStore(RootReducers)
+export default createStore(RootReducers, applyMiddleware(thunk))
