95b8c945f96e9d06e6ff3a171aa2c0d85ce46826
Angel Landkoer <angel.landkoer09@gmail.com>
2023-04-07 10:41:33 -0500

    Importando variables y creando nuevas acciones que cambien el estado

--- src/store/authUser/reducers/authUser.reducer.js
+++ src/store/authUser/reducers/authUser.reducer.js
@@ -1,17 +1,21 @@
-import { SIGN_UP } from "../actions/authUser.action";
+import { SIGN_UP, SIGN_UP_START, SIGN_UP_FAIL, LOGIN, LOGIN_START, LOGIN_FAIL } from "../actions/authUser.action";
 
 const initialState = {
   token: null,
   userId: '',
   isLoading: false,
-  nameToken: ''
+  nameTokenUser: '',
+  existemAccount: false,
 }
 
 export const authReducer = (state = initialState, action) => {
 
-  if (action.type == "@SIGN_UP_START") return { ...state, isLoading: true };
-  if (action.type == SIGN_UP) return { ...state, token: action.token, userId: action.userId, nameToken: action.nameToken };
-  if (action.type == "@SIGN_UP_FAIL") return { ...state, isLoading: false };
+  if (action.type == SIGN_UP_START) return { ...state, isLoading: true };
+  if (action.type == SIGN_UP) return { ...state, isLoading: false, token: action.token, userId: action.userId, nameTokenUser: action.nameToken };
+  if (action.type == SIGN_UP_FAIL) return { ...state, isLoading: false };
+  if (action.type == LOGIN_START) return { ...state, isLoading: true };
+  if (action.type == LOGIN) return { ...state, isLoading: false, existemAccount: action.registered, userId: action.userId };
+  if (action.type == LOGIN_FAIL) return { ...state, isLoading: false };
 
   return state
 }
