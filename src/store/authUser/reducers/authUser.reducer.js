80ad94f2e44c8f9df86ca1de72f805233a27ac78
Angel Landkoer <angel.landkoer09@gmail.com>
2023-04-13 09:58:03 -0500

    Agregando nuevas acciones y propiedad al estado

--- src/store/authUser/reducers/authUser.reducer.js
+++ src/store/authUser/reducers/authUser.reducer.js
@@ -1,4 +1,4 @@
-import { SIGN_UP, SIGN_UP_START, SIGN_UP_FAIL, LOGIN, LOGIN_START, LOGIN_FAIL } from "../actions/authUser.action";
+import { SIGN_UP, SIGN_UP_START, SIGN_UP_FAIL, LOGIN, LOGIN_START, LOGIN_FAIL, RESET_ACCOUNT } from "../actions/authUser.action";
 
 const initialState = {
   token: null,
@@ -6,6 +6,7 @@ const initialState = {
   isLoading: false,
   nameTokenUser: '',
   existemAccount: false,
+  allDataUser: false
 }
 
 export const authReducer = (state = initialState, action) => {
@@ -14,8 +15,9 @@ export const authReducer = (state = initialState, action) => {
   if (action.type == SIGN_UP) return { ...state, isLoading: false, token: action.token, userId: action.userId, nameTokenUser: action.nameToken };
   if (action.type == SIGN_UP_FAIL) return { ...state, isLoading: false };
   if (action.type == LOGIN_START) return { ...state, isLoading: true };
-  if (action.type == LOGIN) return { ...state, isLoading: false, existemAccount: action.registered, userId: action.userId };
+  if (action.type == LOGIN) return { ...state, isLoading: false, existemAccount: action.registered, userId: action.userId, allDataUser: action.allDataUser };
   if (action.type == LOGIN_FAIL) return { ...state, isLoading: false };
+  if (action.type == RESET_ACCOUNT) return { ...state, isLoading: false, token: null, userId: '', nameTokenUser: "", existemAccount: false };
 
   return state
 }
