c45695e24380592a84ca7506d0a4612430f11d03
Angel Landkoer <angel.landkoer09@gmail.com>
2023-04-18 09:48:03 -0500

    Eliminado comentario, importacion no usada y estilos no usados

--- src/screens/MyOrders/MyOrders.jsx
+++ src/screens/MyOrders/MyOrders.jsx
@@ -1,13 +1,11 @@
 import { StyleSheet, View } from "react-native";
 import React from "react";
-import { CustomText } from "../../components/CustomText/CustomText";
 import { themes } from "../../styles/themes";
 import { Orders } from "../../components/Orders/Orders";
 
 export function MyOrders({ navigation }) {
   return (
     <View style={[containerFontBox, primaryBackground]}>
-      {/* Usuario no registrado no pasa se redireccion a Login */}
       <Orders />
     </View>
   );
@@ -17,4 +15,4 @@ const styles = StyleSheet.create({});
 
 const {} = styles;
 
-const { containerFontBox, primaryBackground, text4Xl, primaryColor } = themes;
+const { containerFontBox, primaryBackground } = themes;
