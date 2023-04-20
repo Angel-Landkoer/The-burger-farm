66e6025aef917f326370f31364b7f8b1d4f14039
Angel Landkoer <angel.landkoer09@gmail.com>
2023-04-20 15:54:10 -0500

    Creando nuevo estados iniciales y validado segun las acciones e importando constantes

--- src/store/globalData/reducers/globalData.reducer.js
+++ src/store/globalData/reducers/globalData.reducer.js
@@ -1,4 +1,4 @@
-import { PRODUCT_CATEGORY } from '../actions/globalData.action'
+import { PRODUCT_CATEGORY, CREATE_ORDER, CREATE_ORDER_FAIL, CREATE_ORDER_START, GET_ORDER, GET_ORDER_FAIL, GET_ORDER_START } from '../actions/globalData.action'
 import { datas } from '../../../utils/data'
 
 const data = [
@@ -18,6 +18,10 @@ const initialState = {
   productsCategoryName: data,
   productsData: datas,
   productsCategory: [],
+  orders: false,
+  success: false,
+  isLoading: false,
+  orderId: ""
 }
 
 export const globalDataReducer = (state = initialState, action) => {
@@ -26,5 +30,11 @@ export const globalDataReducer = (state = initialState, action) => {
     const filterCategory = mapCategory.filter((item) => item.category == action.numCategory)
     return { ...state, productsCategory: filterCategory[0] }
   }
+  if (action.type == CREATE_ORDER_START) return { ...state, isLoading: true, }
+  if (action.type == CREATE_ORDER) return { ...state, isLoading: false, success: action.success, orderId: action.id }
+  if (action.type == CREATE_ORDER_FAIL) return { ...state, isLoading: false, success: action.success }
+  if (action.type == GET_ORDER_START) return { ...state, isLoading: true }
+  if (action.type == GET_ORDER) return { ...state, isLoading: false, orders: action.orders }
+  if (action.type == GET_ORDER_FAIL) return { ...state, isLoading: false }
   return state
 }
