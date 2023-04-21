37d9a20eefd79bccf4459edfb06882d8bde0e372
Angel Landkoer <angel.landkoer09@gmail.com>
2023-04-21 16:20:51 -0500

    Creando nueva funcion y añadiendo condicionales con diferentes rutas y agregandola por props al componente

--- src/screens/MyCart/MyCart.jsx
+++ src/screens/MyCart/MyCart.jsx
@@ -1,12 +1,39 @@
 import { Dimensions, StyleSheet, View } from "react-native";
 import React from "react";
+import { useSelector } from "react-redux";
 import { Cart } from "../../components/Cart/Cart";
 import { themes } from "../../styles/themes";
 
 export function MyCart({ navigation }) {
+
+  const registed = useSelector((state) => state.auth.existemAccount);
+  const allDataUser = useSelector((state) => state.auth.allDataUser);
+
+  const allDataUserExistem = allDataUser && allDataUser;
+
+  const userCanAccess = () => {
+    if (!registed) return navigation.navigate("DataDrawer");
+    if (
+      !allDataUserExistem.name &&
+      !allDataUserExistem.lastName &&
+      !allDataUserExistem.email &&
+      !allDataUserExistem.phone
+    )
+      return navigation.navigate("UpdateDataUserStack");
+    if (
+      !allDataUserExistem.address.city &&
+      !allDataUserExistem.address.district &&
+      !allDataUserExistem.address.route &&
+      !allDataUserExistem.address.dataDirection &&
+      !allDataUserExistem.address.additionalInformation
+    )
+      return navigation.navigate("UpdateAddressStack");
+    if (registed) return navigation.navigate("FinalizeOrderStack");
+  };
+
   return (
     <View style={[containerFontBox, primaryBackground, container]}>
-      <Cart />
+      <Cart userCanAccess={userCanAccess} />
     </View>
   );
 }
