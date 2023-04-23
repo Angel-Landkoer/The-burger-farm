86f8dc26310ec95b26edd2850ce66843471aaae6
Angel Landkoer <angel.landkoer09@gmail.com>
2023-04-23 17:50:49 -0500

    Importando datos de instancia temporal y condicionando datos enviados

--- src/screens/MyCart/MyCart.jsx
+++ src/screens/MyCart/MyCart.jsx
@@ -5,11 +5,18 @@ import { Cart } from "../../components/Cart/Cart";
 import { themes } from "../../styles/themes";
 
 export function MyCart({ navigation }) {
-
   const registed = useSelector((state) => state.auth.existemAccount);
   const allDataUser = useSelector((state) => state.auth.allDataUser);
+  const shortTimeUserData = useSelector(
+    (state) => state.auth.shortTimeUserData
+  );
+  const shortTimeAddressData = useSelector(
+    (state) => state.auth.shortTimeAddressData
+  );
 
   const allDataUserExistem = allDataUser && allDataUser;
+  const userData = allDataUser || shortTimeUserData;
+  const addressData = allDataUser.address || shortTimeAddressData;
 
   const userCanAccess = () => {
     if (!registed) return navigation.navigate("DataDrawer");
@@ -28,7 +35,11 @@ export function MyCart({ navigation }) {
       !allDataUserExistem.address.additionalInformation
     )
       return navigation.navigate("UpdateAddressStack");
-    if (registed) return navigation.navigate("FinalizeOrderStack");
+    if (registed)
+      return navigation.navigate("FinalizeOrderStack", {
+        userData,
+        addressData,
+      });
   };
 
   return (
