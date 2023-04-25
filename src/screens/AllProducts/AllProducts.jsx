497077b5f18a327163dd76cf8b3ad6d5a84e82ba
Angel Landkoer <angel.landkoer09@gmail.com>
2023-04-25 11:21:04 -0500

    Eliminando componente y  agregando estilos

--- src/screens/AllProducts/AllProducts.jsx
+++ src/screens/AllProducts/AllProducts.jsx
@@ -17,25 +17,24 @@ export function AllProducts() {
   const findCategory = useSelector((state) => state.data.productsCategory);
 
   return (
-    <ScrollView>
-      <View style={[container, primaryBackground]}>
-        <CustomText
-          style={[title, primaryColor, textCenter]}
-          fontF={"semiBold"}
-        >{`${findCategory.type || "Name Section"}`}</CustomText>
+    <View style={[container, primaryBackground]}>
+      <CustomText
+        style={[title, primaryColor, textCenter]}
+        fontF={"semiBold"}
+      >{`${findCategory.type || "Name Section"}`}</CustomText>
 
-        <RenderList
-          data={findCategory.data || []}
-          component={(item) => <DataCard data={item.item} />}
-          stringKey={(item) => `AllProduct-${item.name}`}
-          numColumns={3}
-          contentContainerStyle={{
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        />
-      </View>
-    </ScrollView>
+      <RenderList
+        data={findCategory.data || []}
+        component={(item) => <DataCard data={item.item} />}
+        stringKey={(item) => `AllProduct-${item.name}`}
+        styles={{ width: widthPixel(320), height: "100%" }}
+        numColumns={3}
+        contentContainerStyle={{
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      />
+    </View>
   );
 }
 
@@ -44,10 +43,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
 
-    minHeight: heightPixel(480),
+    height: "100%",
+    width: "100%",
   },
   title: {
-    fontSize: fontPixel(18),
+    fontSize: fontPixel(20),
 
     paddingVertical: pixelSizeVertical(10),
     paddingHorizontal: pixelSizeHorizontal(10),
