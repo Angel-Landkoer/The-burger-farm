ad5bcf27fdf8efb29d25ecb328ce5dc621512be1
Angel Landkoer <angel.landkoer09@gmail.com>
2023-04-26 17:18:48 -0500

    Implementando persistencia de datos locales

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { MainNavigation } from './src/navigation/MainNavigation';
 import { Provider } from 'react-redux'
-import store from './src/store/index'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, storePersisted } from './src/store/index'
 
 export default function App() {
 
   return (
     <Provider store={store}>
-      <MainNavigation />
+      <PersistGate loading={null} persistor={storePersisted}>
+        <MainNavigation />
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,24 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { persistReducer, persistStore } from 'redux-persist'
 import { cartReducer } from './cartSistem/reducers/cartSistem.reducer'
 import { globalDataReducer } from './globalData/reducers/globalData.reducer'
 import { authReducer } from './authUser/reducers/authUser.reducer'
 
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage
+}
+
 const RootReducers = combineReducers({
   cart: cartReducer,
   data: globalDataReducer,
-  auth: authReducer
+  auth: authReducer,
 })
 
-export default createStore(RootReducers, applyMiddleware(thunk))
+const persistedReducer = persistReducer(persistConfig, RootReducers)
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk))
+
+export const storePersisted = persistStore(store)
