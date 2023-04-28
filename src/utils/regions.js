0d6a13307f77dfa4dd5c737eba40261f3bbda0e4
Angel Landkoer <angel.landkoer09@gmail.com>
2023-04-28 15:48:10 -0500

    Creando un mockup de datos y exportandolos

---
+++ src/utils/regions.js
@@ -0,0 +1,74 @@
+export const regions = [
+  {
+    name: "Colombia",
+    region: {
+      latitude: 4.5709,
+      longitude: -74.2973,
+      latitudeDelta: 10,
+      longitudeDelta: 10,
+    },
+  },
+  {
+    name: "Chile",
+    region: {
+      latitude: -35.6751,
+      longitude: -71.543,
+      latitudeDelta: 10,
+      longitudeDelta: 10,
+    },
+  },
+  {
+    name: "Bolivia",
+    region: {
+      latitude: -16.2902,
+      longitude: -63.5887,
+      latitudeDelta: 10,
+      longitudeDelta: 10,
+    },
+  },
+  {
+    name: "Argentina",
+    region: {
+      latitude: -34.6037,
+      longitude: -58.3816,
+      latitudeDelta: 10,
+      longitudeDelta: 10,
+    },
+  },
+  {
+    name: "Uruguay",
+    region: {
+      latitude: -34.9011,
+      longitude: -56.1645,
+      latitudeDelta: 10,
+      longitudeDelta: 10,
+    },
+  },
+  {
+    name: "Peru",
+    region: {
+      latitude: -9.1898,
+      longitude: -75.0152,
+      latitudeDelta: 10,
+      longitudeDelta: 10,
+    },
+  },
+  {
+    name: "Mexico",
+    region: {
+      latitude: 23.6345,
+      longitude: -102.5528,
+      latitudeDelta: 10,
+      longitudeDelta: 10,
+    },
+  },
+  {
+    name: "Canada",
+    region: {
+      latitude: 56.1304,
+      longitude: -106.3468,
+      latitudeDelta: 10,
+      longitudeDelta: 10,
+    },
+  },
+];
