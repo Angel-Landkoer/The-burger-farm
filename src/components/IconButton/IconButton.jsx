252d3f7cbb1d3885bbb2c154957ef60cccbe4b0e
Angel Landkoer <angel.landkoer09@gmail.com>
2023-05-03 10:53:08 -0500

    Dependiendo funcion

--- src/components/IconButton/IconButton.jsx
+++ src/components/IconButton/IconButton.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { Pressable } from "react-native";
-import { useNavigation } from "@react-navigation/native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { themes } from "../../styles/themes";
 
-export function IconButton() {
-  const { goBack } = useNavigation();
-  const goToBack = () => goBack();
+export function IconButton({ callback }) {
 
   return (
-    <Pressable onPress={goToBack}>
+    <Pressable onPress={callback}>
       <MaterialCommunityIcons
         name="arrow-left-circle"
         size={44}
